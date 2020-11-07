a3a933e5604b8ac0a88f3ef266abc43c5e2a24cf
Sean McElwain <semcelw@gmail.com>
2020-11-07 10:33:02 -0500

    change how we get home dir

--- network_switch.py
+++ network_switch.py
@@ -8,8 +8,8 @@ app = typer.Typer()
 @app.command()
 def wifi():
     subprocess.check_call(["sudo", "systemctl", "stop", "hostapd"])
-    shutil.copy(Path("~/scripts/dhcpcd.conf.wifi").home(), Path("/etc/dhcpcd.conf"))
-    shutil.copy(Path("~/scripts/dnsmasq.conf.wifi").home(), Path("/etc/dnsmasq.conf"))
+    shutil.copy(Path("~/scripts/dhcpcd.conf.wifi").expanduser(), Path("/etc/dhcpcd.conf"))
+    shutil.copy(Path("~/scripts/dnsmasq.conf.wifi").expanduser(), Path("/etc/dnsmasq.conf"))
     subprocess.check_call(["sudo", "systemctl", "restart", "dhcpcd"])
     subprocess.check_call(["sudo", "systemctl", "restart", "dnsmasq"])
 
@@ -18,8 +18,8 @@ def wifi():
 @app.command()
 def access_point():
     subprocess.check_call(["sudo", "systemctl", "start", "hostapd"])
-    shutil.copy(Path("~/scripts/dhcpcd.conf.access").home(), Path("/etc/dhcpcd.conf"))
-    shutil.copy(Path("~/scripts/dnsmasq.conf.access").home(), Path("/etc/dnsmasq.conf"))
+    shutil.copy(Path("~/scripts/dhcpcd.conf.access").expanduser(), Path("/etc/dhcpcd.conf"))
+    shutil.copy(Path("~/scripts/dnsmasq.conf.access").expanduser(), Path("/etc/dnsmasq.conf"))
     subprocess.check_call(["sudo", "systemctl", "restart", "dhcpcd"])
     subprocess.check_call(["sudo", "systemctl", "restart", "dnsmasq"])
 
