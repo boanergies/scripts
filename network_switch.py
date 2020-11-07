0cb2bb8b938c9c6d3c6336094a0d19e5d6f1c2f3
Sean McElwain <semcelw@gmail.com>
2020-11-07 10:29:55 -0500

    fix script

--- network_switch.py
+++ network_switch.py
@@ -8,16 +8,20 @@ app = typer.Typer()
 @app.command()
 def wifi():
     subprocess.check_call(["sudo", "systemctl", "stop", "hostapd"])
-    shutil.copy(Path("~/scripts/dhcpcd.conf.wifi").home(), Path("/etc/dhpcd.conf"))
+    shutil.copy(Path("~/scripts/dhcpcd.conf.wifi").home(), Path("/etc/dhcpcd.conf"))
     shutil.copy(Path("~/scripts/dnsmasq.conf.wifi").home(), Path("/etc/dnsmasq.conf"))
+    subprocess.check_call(["sudo", "systemctl", "restart", "dhcpcd"])
+    subprocess.check_call(["sudo", "systemctl", "restart", "dnsmasq"])
 
 
 
 @app.command()
 def access_point():
     subprocess.check_call(["sudo", "systemctl", "start", "hostapd"])
-    shutil.copy(Path("~/scripts/dhcpcd.conf.access").home(), Path("/etc/dhpcd.conf"))
+    shutil.copy(Path("~/scripts/dhcpcd.conf.access").home(), Path("/etc/dhcpcd.conf"))
     shutil.copy(Path("~/scripts/dnsmasq.conf.access").home(), Path("/etc/dnsmasq.conf"))
+    subprocess.check_call(["sudo", "systemctl", "restart", "dhcpcd"])
+    subprocess.check_call(["sudo", "systemctl", "restart", "dnsmasq"])
 
 if __name__ == "__main__":
     app()
